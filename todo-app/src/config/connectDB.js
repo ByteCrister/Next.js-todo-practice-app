3f4c281d5b922a4947a97ffb929647dd7a834c53
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2024-12-13 22:30:45 +0600

    changed in db - 2

--- todo-app/src/config/connectDB.js
+++ todo-app/src/config/connectDB.js
@@ -1,16 +1,18 @@
 import mongoose from 'mongoose';
 
-let isConnected = false;
+let isConnected = false; // To track the connection state
 
 const connectDB = async () => {
-  if (isConnected) return;
+  if (isConnected) {
+    console.log('Database already connected');
+    return;
+  }
   try {
     const db = await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI + '/test', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      dbName: "test", 
     });
     isConnected = db.connections[0].readyState === 1;
-    console.log('Database connected');
+    console.log('Database connected successfully');
   } catch (error) {
     console.error('Database connection failed:', error);
     throw new Error('Database connection failed');
